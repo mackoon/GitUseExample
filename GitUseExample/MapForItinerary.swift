e0094fc063056b3dc751952be34c2e74775f948d
Stuart Hearn <mackoon@icloud.com>
2018-04-24 11:24:11 +0100

    First commit on new branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Stuart Hearn on 24/04/2018.
+//  Copyright © 2018 Stuart Hearn. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
