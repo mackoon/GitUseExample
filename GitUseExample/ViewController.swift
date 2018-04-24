ba62528a0e32f7ac00ef806a2c7ca02f44bd3c82
Stuart Hearn <mackoon@icloud.com>
2018-04-24 10:35:07 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Stuart Hearn on 24/04/2018.
+//  Copyright © 2018 Stuart Hearn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
